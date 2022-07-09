ef1da4e266fbc83e491acf8d653624f740a0b219
Roy Veshovda <roy@veshovda.no>
2022-07-09 10:07:44 +0200

    status LED

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 
 # Update + upgrade
 sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install linux-modules-extra-raspi -y
 
 # Docker
 #curl -sSL https://get.docker.com | sh
@@ -15,6 +14,7 @@ curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644
 sudo sh -c "echo 'gpu_mem=256' >> /boot/config.txt"
 sudo sh -c "echo 'enable_uart=1' >> /boot/config.txt"
 sudo sh -c "echo 'dtoverlay=gpio-shutdown,gpio_pin=3' >> /boot/config.txt"
+sudo sh -c "echo 'dtoverlay=gpio-led,gpio=17,trigger=default-on,label=statusled0' >> /boot/config.txt"
 
 # Zerotier
 curl -s https://install.zerotier.com | sudo bash
@@ -23,3 +23,6 @@ curl -s https://install.zerotier.com | sudo bash
 #sudo sh -c "echo 'cgroup_enable=cpuset' >> /boot/cmdline.txt"
 #sudo sh -c "echo 'cgroup_enable=memory' >> /boot/cmdline.txt"
 #sudo sh -c "echo 'cgroup_memory=1' >> /boot/cmdline.txt"
+
+# Update + upgrade
+sudo apt-get update && sudo apt-get upgrade -y
