5a298dc138f8a6596328d81c1fe28f55f4a7a16a
Roy Veshovda <roy@veshovda.no>
2022-07-14 19:11:06 +0200

    Apply configmap

--- install_rpiuav.sh
+++ install_rpiuav.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+kubectl apply -f https://gitlab.com/got.vision/rpiuav/-/raw/main/config/rpi4-configmap.yaml?inline=false
 kubectl apply -f https://gitlab.com/got.vision/rpiuav/-/raw/main/deployments/rpi4/flux-system/gotk-components.yaml?inline=false
 flux create source git flux-infra --url=https://gitlab.com/got.vision/rpiuav --branch=main --interval=1m
 flux create kustomization rpi4 --source=flux-infra --path="./deployments/rpi4" --prune=true --interval=5m
