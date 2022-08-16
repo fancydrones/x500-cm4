32c1201e0f53d7edfc04c69474fe89ac7856a226
Roy Veshovda <roy@veshovda.no>
2022-08-16 10:51:59 +0200

    fix path

--- install/install_rpiuav.sh
+++ install/install_rpiuav.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-kubectl apply -f https://raw.githubusercontent.com/fancydrones/x500-rpi4/main/deployments/rpi4/apps/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/fancydrones/x500-rpi4/main/deployments/apps/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/fancydrones/x500-rpi4/main/config/rpi4-configmap.yaml
-kubectl apply -f https://raw.githubusercontent.com/fancydrones/x500-rpi4/main/deployments/rpi4/flux-system/gotk-components.yaml
+kubectl apply -f https://raw.githubusercontent.com/fancydrones/x500-rpi4/main/deployments/flux-system/gotk-components.yaml
 flux create source git flux-infra --url=https://github.com/fancydrones/x500-rpi4 --branch=main --interval=1m
 flux create kustomization rpi4 --source=flux-infra --path="./deployments" --prune=true --interval=5m
