f0b4d4d5a7a4580fd398f703459f138b86a80671
Roy Veshovda <roy@veshovda.no>
2022-09-24 07:20:18 +0200

    Bump to newest version again

--- apps/companion/lib/companion_web.ex
+++ apps/companion/lib/companion_web.ex
@@ -92,6 +92,8 @@ defmodule CompanionWeb do
       # Import LiveView and .heex helpers (live_render, live_patch, <.form>, etc)
       import Phoenix.LiveView.Helpers
 
+      import Phoenix.Component
+
       # Import basic rendering functionality (render, render_layout, etc)
       import Phoenix.View
 
