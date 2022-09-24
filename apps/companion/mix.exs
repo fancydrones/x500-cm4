48973757c29edc4be1e75be0ebe8e9e570990155
Roy Veshovda <roy@veshovda.no>
2022-09-24 13:16:20 +0200

    1.14 again

--- apps/companion/mix.exs
+++ apps/companion/mix.exs
@@ -5,7 +5,7 @@ defmodule Companion.MixProject do
     [
       app: :companion,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
