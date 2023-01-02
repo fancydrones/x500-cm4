2e4186f9ad4ca49c6f1bc2f70be8a4f39dd1da50
Roy Veshovda <roy@veshovda.no>
2023-01-02 16:24:16 +0100

    Some magic version number. 3 is same as PX4 is sending
    Signed-off-by: Roy Veshovda <roy@veshovda.no>

--- apps/announcer/mavlinkcamera.py
+++ apps/announcer/mavlinkcamera.py
@@ -232,7 +232,7 @@ class MavlinkCameraManager(threading.Thread):
             mavutil.mavlink.MAV_AUTOPILOT_GENERIC,
             0,
             mavutil.mavlink.MAV_STATE_STANDBY,
-            2
+            3
         )
 
     def makestring(self, string, size):
