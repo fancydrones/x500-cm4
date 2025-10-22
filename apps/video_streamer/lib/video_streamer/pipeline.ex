79a9a8a04cd38351da282610f3836abe1477ed23
Roy Veshovda <roy@veshovda.no>
2025-10-22 14:29:35 +0200

    Fix module reference for fake sink in video streaming pipeline

--- apps/video_streamer/lib/video_streamer/pipeline.ex
+++ apps/video_streamer/lib/video_streamer/pipeline.ex
@@ -22,7 +22,7 @@ defmodule VideoStreamer.Pipeline do
       |> child(:h264_parser, Membrane.H264.Parser)
       |> child(:rtp_payloader, Membrane.RTP.H264.Payloader)
       # Temporary sink for Phase 1 testing - will be replaced with RTSP output in Phase 2
-      |> child(:fake_sink, Membrane.Element.Fake.Sink.Buffers)
+      |> child(:fake_sink, Membrane.Fake.Sink.Buffers)
     ]
 
     {[spec: spec], %{}}
